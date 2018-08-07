2f86e5d81c75fdfb4a744ce37ba575ed91988a1e
Alptekin Orbay <alptekin.orbay@gmail.com>
2018-08-07 13:37:20 +0300

    faster extracter implemented.

--- CreateDataset.py
+++ CreateDataset.py
@@ -1,4 +1,4 @@
-from Helper import sum_time, convert_delta , sub_time, find_places, create_dataset
+from Helper import sum_time, convert_delta , sub_time, find_places, create_dataset, safe_create, big_print
 
 #########
 #########
@@ -7,20 +7,34 @@ video_dif_list = ['01:28','28:59','55:51','1:24:31','1:45:47','2:11:36','2:38:32
 video_len = ['27:33','26:55','28:43','21:19','25:51','27:00','29:11']
 video_list = ['MVI_8191.MOV','MVI_8192.MOV','MVI_8193.MOV','MVI_8194.MOV','MVI_8195.MOV','MVI_8196.MOV','MVI_8197.MOV']
 video_list = ['titanic/SL/' + video for video in video_list]
-for i in range(1,len(video_dif_list)):
+for i in range(1, len(video_dif_list)):
     sub_time(video_dif_list[i],video_dif_list[i-1])
-    print((convert_delta(video_len[i-1])))
-    print('----------------------------------')
+    #print((convert_delta(video_len[i-1])))
+    #print('----------------------------------')
 video_dif_list_delta = []
 for t in video_dif_list:
     video_dif_list_delta.append(convert_delta(t))
-print(sum_time(video_len))
+#print(sum_time(video_len))
 #######
 #######
 #######
+
+words = []
 target_film = 'titanic'
 subs_file = target_film + '/substitle.srt'
 sl_video_path = target_film + '\SL'
+print('Words:')
+for w in words:
+    print(w)
+big_print('Data Prep Starts.')
 
-p = find_places(None, video_dif_list_delta, video_list, subs_file)
-create_dataset(p, 'train', target_film)
+train, dev, test = find_places(words, video_dif_list_delta, video_list, subs_file)
+safe_create('Dataset')
+safe_create('Data')
+
+create_dataset(train, 'train', target_film)
+print('Train ready.')
+create_dataset(dev, 'dev', target_film)
+print('Dev ready.')
+create_dataset(test, 'test', target_film)
+print('Test ready.')
--- Helper.py
+++ Helper.py
@@ -4,7 +4,7 @@ import shutil
 import os
 import random
 import cv2
-
+import subprocess
 
 def sum_time(time_list):
     total = datetime.timedelta()
@@ -25,7 +25,7 @@ def convert_delta(time):
 
 def sub_time(time1, time2):
     d = convert_delta(time1) - convert_delta(time2)
-    print(str(d))
+    #print(str(d))
 
 
 def select_video(start, video_list, video_dif_list_delta):
@@ -39,7 +39,7 @@ def select_video(start, video_list, video_dif_list_delta):
 
 
 def word_in(words, sentence):
-    if words is None:
+    if len(words) == 0:
         return True
     for word in words:
         if word in sentence:
@@ -64,10 +64,10 @@ def safe_create(path):
         shutil.rmtree(path)
         print(path, 'Deleted')
         os.mkdir(path)
-        print(path, 'created.')
+        #print(path, 'created.')
     except:
         os.mkdir(path)
-        print(path, 'created.')
+        #print(path, 'created.')
 
 
 def find_places(words, video_dif_list_delta, video_list, subs_file):
@@ -81,79 +81,110 @@ def find_places(words, video_dif_list_delta, video_list, subs_file):
                 end = convert_delta(prompt.interval[1].replace(',', '.'))
                 duration = end - start
                 vd, vd_start = select_video(start, video_list, video_dif_list_delta)
-                vd_end = vd_start.total_seconds()+duration.total_seconds()
+                vd_end = duration.total_seconds() + 1
                 sb = [vd, index, vd_start.total_seconds(), vd_end, prompt.sentence]
                 index += 1
                 places.append(sb)
-    return places
-    ##################
-
-    # random.shuffle(places)
-    # size = len(places)
-    # train_set = places[:int(size*0.7)]
-    # #dev_set = places[int(size*0.7):int(size*0.85)]
-    # #test_set = places[int(size*0.85):size]
-    # #write_data(train_set,'train.txt')
-    # #write_data(test_set,'test.txt')
-    # #write_data(dev_set,'dev.txt')
-    # return train_set #,dev_set,test_set
+    random.shuffle(places)
+    size = len(places)
+    train_set = places[:int(size*0.7)]
+    dev_set = places[int(size*0.7):int(size*0.85)]
+    test_set = places[int(size*0.85):size]
+    print('Number of sentences:', size)
+    return train_set, dev_set, test_set
 
 
 def get_frames(video_path, start_time_ms, stop_time_ms, resolution=227, cut_slides=80):
     frames = []
-    count = 0
     success = True
 
     vidcap = cv2.VideoCapture(video_path)
-
+    print('Video extraction starts.')
     while success and vidcap.get(cv2.CAP_PROP_POS_MSEC) < start_time_ms:
         success, image = vidcap.read()
-
+    print('Video places found.')
     while success and vidcap.get(cv2.CAP_PROP_POS_MSEC) <= stop_time_ms:
         success, image = vidcap.read()
         if cut_slides is not None:
             image = image[:, 80:-80]
         if resolution is not None:
-            image = cv2.resize(image, (resolution,resolution))
+            image = cv2.resize(image, (resolution, resolution))
         frames.append(image)
-
+    print('Video extraction ends.')
     return frames
 
 
 def write_subs(sentence, f):
+    s = ''
     for word in sentence:
         f.write(word)
         f.write(" ")
+        s += word
+        s += ' '
+    f.write(".")
     f.write("\n")
+    return s
 
 
 def write_frame(frames, path):
     for i, f in enumerate(frames):
         d = 0.0001 * i
-        cv2.imwrite(path + 'frame' + str(d)[2:7] + '.png', f)
+        cv2.imwrite(path + 'frame' + str(d)[2:6] + '.png', f)
+
+
+def big_print(s, message=None):
+    print()
+    print('===================')
+    print('===================')
+    print(s)
+    if message is not None:
+        print('===================')
+        print(message)
+    print('===================')
+    print('===================')
+
+    print()
+
+
+def ffmpeg_call(video, start, duration, resolution, path):
+    res = str(resolution)+'x'+str(resolution)
+    output = path + 'thumb%05d.png'
+    st = str(datetime.timedelta(seconds=start))
+    dr = str(datetime.timedelta(seconds=duration))
+    command = ['ffmpeg', "-ss", st, "-i", video, "-s", res, "-t", dr, '-f', 'image2', output, '-loglevel', 'quiet']
+    #print(' '.join(command))
+    subprocess.call(command)
+    # command = ['ffmpeg', '-ss', '00:00:50', '-i', 'titanic/SL/MVI_8196.MOV', '-s', '227x227', '-t', '00:00:02',
+    #            '-f','image2', 'thumb%05d.jpg']
+    # subprocess.call(command)
 
 
 def create_dataset(data, type,target_film, new_resolution = 227, cut_sides = 80):
-    safe_create('Dataset')
-    safe_create('Data')
     dataset_path_tr = 'Dataset/' + type + '.tr'
     dataset_path_sign = 'Dataset/' + type + '.sign'
     file_sign = open(dataset_path_sign, 'w')
     file_tr = open(dataset_path_tr, 'w')
     for i, vd in enumerate(data):
-        frames = get_frames(vd[0], vd[2] * 1000, vd[3] * 1000)
-        if len(frames) < 1:
-            continue
-        pwd = os.getcwd()
+        # frames = get_frames(vd[0], vd[2] * 1000, vd[3] * 1000)
+        # if len(frames) < 1:
+        #     continue
         frame_path = os.getcwd() + '/Data/' + target_film + '_' + str(vd[1]) + '/'
         sentence = vd[-1]
         ################
         ################
-        ################
         safe_create(frame_path)
-        write_frame(frames, frame_path)
+        ffmpeg_call(vd[0], vd[2], vd[3], 227, frame_path)
+        if len(os.listdir(frame_path)) == 0:
+            msg = str(vd[0]) +'  ' + str(vd[2]) +'  ' + str(vd[3]) +'  '+ ' '.join(vd[-1])
+            big_print('Empty Directory', msg)
+            shutil.rmtree(frame_path)
+            continue
+        ################
+        ################
+        ################
+        # write_frame(frames, frame_path)
         file_sign.write(frame_path + "\n")
-        write_subs(sentence, file_tr)
+        #print(write_subs(sentence, file_tr))
         ################
         ################
         ################
